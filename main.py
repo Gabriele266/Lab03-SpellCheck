40e706fb357e2bc2805593ff188a039c597715cb
Informatica.io <informaticaio.info@gmail.com>
2026-03-17 10:18:58 +0100

    Add code skelethon

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-import spellchecker
 
-sc = spellchecker.SpellChecker()
+def main():
+    print("Hello World")
 
-while(True):
-    sc.printMenu()
-
-    txtIn = input()
-    # Add input control here!
-
-    if int(txtIn) == 1:
-        print("Inserisci la tua frase in Italiano\n")
-        txtIn = input()
-        sc.handleSentence(txtIn,"italian")
-        continue
-
-    if int(txtIn) == 2:
-        print("Inserisci la tua frase in Inglese\n")
-        txtIn = input()
-        sc.handleSentence(txtIn,"english")
-        continue
-
-    if int(txtIn) == 3:
-        print("Inserisci la tua frase in Spagnolo\n")
-        txtIn = input()
-        sc.handleSentence(txtIn,"spanish")
-        continue
-
-    if int(txtIn) == 4:
-        break
-
+if __name__ == "__main__":
+    main()
 
